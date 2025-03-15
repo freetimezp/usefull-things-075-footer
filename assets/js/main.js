22f19f279550e1c2465240c4cdeb38b608d50ffe
freetimezp <kraftukrnet@ukr.net>
2025-03-15 19:27:34 +0200

    02. usefull-things-075-footer. UI. GSAP animation on scroll.

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+
+gsap.from(".fixed__footer", {
+    y: -500,
+    scrollTrigger: {
+        trigger: ".content",
+        start: "bottom bottom",
+        scrub: true
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
